23d9d5398d2c898693d3e35e6b762ffff79b6e4b
BoanergesJunior <boanerges_junior@hotmail.com>
2020-11-17 19:07:10 -0300

    Subindo arquivos iniciais

---
+++ A.java
@@ -0,0 +1,25 @@
+package eng_software_git;
+
+public class A {
+	
+	private int A1;
+	private float A2;
+
+	public int getA1() {
+		return A1;
+	}
+
+	public float getA2() {
+		return A2;
+	}
+	
+	public void MA1() {
+		System.out.print("MA1");
+	}
+	
+	public void MA2() {
+		System.out.print("MA2");
+	}
+	
+	
+}
---
+++ B.java
@@ -0,0 +1,23 @@
+package eng_software_git;
+
+public class B {
+	private int B1;
+	private float B2;
+	
+	public int getB1() {
+		return B1;
+	}
+
+	public float getB2() {
+		return B2;
+	}
+	
+	public void MB1() {
+		System.out.print("MB1");
+	}
+	
+	public void MA2() {
+		System.out.print("MB2");
+	}
+	
+}
